49497d7ab0568f46bcf2499a33ef3782f8342f0c
Ruth Ironside <ruthironside@live.com>
2020-10-24 12:11:42 +0100

    add and remove guests

--- classes/room.py
+++ classes/room.py
@@ -2,3 +2,16 @@ class Room:
     def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
+        self.guests = []
+
+    # def add_song_to_room(self, song):
+    #     self.rooms.append(song)
+
+    def guest_count(self):
+        return len(self.guests)
+
+    def add_guest(self, guest):
+        self.guests.append(guest)
+
+    def remove_guest(self, guest):
+        self.guests.remove(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,6 +1,7 @@
 import unittest
 from classes.room import *
 from classes.guest import *
+from classes.song import *
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
@@ -18,5 +19,40 @@ class TestRoom(unittest.TestCase):
         self.guest_4 = Guest("BMO", 1000)
         guests = [self.guest_1, self.guest_2, self.guest_3, self.guest_4]
 
+        self.song_1 = Song("Wannabe", "Spice Girls", "Pop")
+        self.song_2 = Song("Angels", "Robbie Williams", "Pop")
+        self.song_3 = Song("Baggy Trousers", "Madness", "Ska")
+        self.song_4 = Song("Country House", "Blur", "Brit Pop")
+        self.song_5 = Song("Should I Stay Or Should I Go", "The Clash", "Rock")
+        self.song_6 = Song("Achy Breaky Heart", "Billy Ray Cyrus", "Country")
+        songs = [self.song_1, self.song_2, self.song_3, self.song_4, self.song_5, self.song_6]
+
+
     def test_room_has_name(self):
         self.assertEqual("one", self.room.name)
+
+    def test_room_has_capacity(self):
+        self.assertEqual(12, self.room_2.capacity)
+
+    def test_song_has_name(self):
+        self.assertEqual("Wannabe", self.song_1.name)
+
+    def test_starts_with_no_guests(self):
+        self.assertEqual(0, self.room.guest_count())
+
+    def test_can_add_guest_to_room(self):
+        guest = Guest("Lumpy Space Princess", 14)
+        self.room.add_guest(guest)
+        self.assertEqual(1, self.room.guest_count())
+
+    def test_can_remove_guest_from_room(self):
+        guest = Guest("Lumpy Space Princess", 14)
+        self.room.add_guest(guest)
+        self.room.remove_guest(guest)
+        self.assertEqual(0, self.room.guest_count())
+    
+
+    # def test_add_song_to_room(self):
+    #     song = Song("Ring of Fire", "Johnny Cash", "Country")
+    #     self.song.add_song(song)
+    #     self.assertEqual(1, self.room.add_song())
